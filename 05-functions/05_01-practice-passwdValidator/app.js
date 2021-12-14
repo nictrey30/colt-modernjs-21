15d4ec058cff8380a38b555d1471c36951602c34
Traian <traian30y@yahoo.co.uk>
2021-12-14 13:19:17 +0000

    ex1 05-fucntions

---
+++ 05-functions/05_01-practice-passwdValidator/app.js
@@ -0,0 +1,20 @@
+// 05-functions practice 1 - passwordValidator
+// Write a isValidPassword function
+// It accepts 2 arguments: password and username
+// Password must:
+// - be at least 8 char
+// - cannot contain spaces
+// - cannot contain the username
+// If all req are met return true, otherwise return false
+
+// isValidPassword('89Fjj1nms', 'dogLuvr'); //true
+// isValidPassword('dogLuvr123!', 'dogLuvr'); //false
+
+function isValidPassword(pass, user) {
+  if (pass && user)
+    return pass.length > 8 && pass.indexOf(' ') === -1 && !pass.includes(user);
+  return false;
+}
+console.log(isValidPassword('89Fjj1nms', 'dogLuvr')); //true
+console.log(isValidPassword('dogLuvr123!', 'dogLuvr')); //false
+console.log(isValidPassword('hello1', 'dogLuvr')); //false
--- 05-functions/app.js
+++ 05-functions/app.js
@@ -5,7 +5,7 @@ function grumpus() {
   console.log('LEAVE ME ALONE!!!');
 }
 // STEP 2: Call the function:
-grumpus();
+// grumpus();
 
 // dice roll function
 function rollDie() {
@@ -15,4 +15,26 @@ function rollDie() {
 function throwDice(rolls) {
   for (let i = 0; i < rolls; i++) console.log(`rolled ${rollDie()}`);
 }
-throwDice(3);
+// throwDice(3);
+
+// functions with multiple arguments
+function square(num) {
+  return num * num;
+}
+function sum(x, y) {
+  return x + y;
+}
+function divide(a, b) {
+  return a / b;
+}
+function isPurple(color) {
+  return color.toLowerCase() === 'purple';
+}
+function containsPurple(arr) {
+  for (let color of arr) {
+    if (color.toLowerCase() === 'purple') {
+      return true;
+    }
+  }
+  return false;
+}
