6746133072cb6e39eb8d3d36d927f46a3f9ddc74
Traian <traian30y@yahoo.co.uk>
2021-12-18 10:24:47 +0000

    06 functions update

--- 06-functions-adv/app-05-func_as_arg.js
+++ 06-functions-adv/app-05-func_as_arg.js
@@ -2,3 +2,38 @@
 // functions that operate on/with other functions. They can:
 // - accept other functions as arguments
 // - return a function
+// This function accepts another function as an argument
+function callThreeTimes(func) {
+  //And calls it 3 times:
+  func();
+  func();
+  func();
+}
+
+function cry() {
+  console.log("BOO HOO I'M SO SAD!");
+}
+
+function rage() {
+  console.log('I HATE EVERYTHING!');
+}
+
+function repeatNTimes(action, num) {
+  // call action (a function) num number of times
+  for (let i = 0; i < num; i++) {
+    action();
+  }
+}
+
+repeatNTimes(rage, 4);
+
+// Accepts 2 functions as arguments
+// Randomly selects 1 to execute
+function pickOne(f1, f2) {
+  let rand = Math.random();
+  if (rand < 0.5) {
+    f1();
+  } else {
+    f2();
+  }
+}
