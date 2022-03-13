4b19f4a7739b05df2bdd2c6c7850de8a71d2e9a6
Traian <traian30y@yahoo.co.uk>
2022-03-13 06:09:17 +0000

    UPdate

--- 09-obj-methods/app-03-this.js
+++ 09-obj-methods/app-03-this.js
@@ -14,5 +14,14 @@ const person = {
   },
   printBio() {
     console.log(`${this.fullName()} is a person!`);
+  },
+  // THIS - invocation context
+  // The value of THIS depends on the invocation context of the function it is used in
+  // arrow functions don't get their own version of THIS
+  laugh: () => {
+    console.log(this);
+    console.log(`${this.nickName} says HAHAHA`);
   }
 };
+
+person.laugh(); // undefined says HAHAHA
