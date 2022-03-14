d6f870ff6c5ee4a6a72b97ca4159940cf4b2dc33
Traian <traian30y@yahoo.co.uk>
2022-03-14 10:02:57 +0000

    09-obj-methods THIS

---
+++ 09-obj-methods/app-04-annoyomatic.js
@@ -0,0 +1,29 @@
+const annoyer = {
+  phrases: [
+    'literally',
+    'cray cray',
+    "I can't even",
+    'Totes!',
+    'YOLO',
+    "Can't Stop, Won't Stop"
+  ],
+  pickPhrase() {
+    const { phrases } = this;
+    // random index from the phrases array
+    const idx = Math.floor(Math.random() * phrases.length);
+    return phrases[idx];
+  },
+  start() {
+    // will not work, because this will get the context of setInterval
+    // setInterval(function () {
+    //   console.log(this.pickPhrase());
+    // }, 2000);
+    // arrowfunctions don't get their own this
+    this.timerID = setInterval(() => {
+      console.log(this.pickPhrase());
+    }, 2000);
+  },
+  stop() {
+    clearInterval(this.timerID);
+  }
+};
