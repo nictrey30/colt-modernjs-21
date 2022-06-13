e01a249eeecda21177cf242797cf1ac72be5d80a
Traian <traian30y@yahoo.co.uk>
2022-06-13 19:16:01 +0100

    update from win11 06.22

--- 13-async/app.js
+++ 13-async/app.js
@@ -1,27 +1,26 @@
 const repeat = (str, times) => {
-	let result = '';
-	for (let i = 0; i < times; i++) {
-		result += str;
-	}
-	return result;
+  let result = '';
+  for (let i = 0; i < times; i++) {
+    result += str;
+  }
+  return result;
 };
 
-const scream = (str) => {
-	return str.toUpperCase() + '!!!';
-};
+const scream = (str) => str.toUpperCase() + '!!';
 
 const getRantText = (phrase) => {
-	let text = scream(phrase);
-	let rant = repeat(text, 8);
-	return rant;
+  let text = scream(phrase);
+  let rant = repeat(text, 3);
+  return rant;
 };
 
 const makeRant = (phrase, el) => {
-	const h1 = document.createElement('h1');
-	h1.innerText = getRantText(phrase);
-	el.appendChild(h1);
+  const h2 = document.createElement('h2');
+  h2.innerText = getRantText(phrase);
+  el.appendChild(h2);
 };
-console.log('HELLO!');
 
-makeRant('I hate mayonnaise', document.body);
-// makeRant('if you have to cough, please cover your mouth', document.body);
+console.log('hello');
+
+makeRant('I hate mayo', document.body);
+// makeRant('I like rice', document.body);
